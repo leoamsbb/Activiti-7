868402938eef1f86b4e6e8bdf8fe7ecd5d27e4e8
Mrunal Badhe <mrunal.badhe@springernature.com>
2020-01-20 10:45:20 +0530

    Initial Commit for Sample Runtime Bundle

---
+++ sample-activiti7-runtime-bundle/src/test/java/org/activiti/training/sampleactiviti7runtimebundle/SampleActiviti7RuntimeBundleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.activiti.training.sampleactiviti7runtimebundle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleActiviti7RuntimeBundleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
